957bd578af85596e438ee03d9c8d726b883427d2
oliveiraFab16 <fabriciocarvalho932@gmail.com>
2020-10-09 18:37:26 -0300

    numeroPorExtenso

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Humanizer;
+
+namespace NumeralExtenso
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("--- Humanizando Números!");
+            int numero = 321651321;
+            string numeroPorExtenso = numero.ToWords();
+            Console.WriteLine($"{numero} --> {numeroPorExtenso}");
+
+        }
+    }
+}
